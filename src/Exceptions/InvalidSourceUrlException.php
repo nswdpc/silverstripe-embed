2ac68c587e805c2e54227b4ae6c46bc7075f6089
JamesDPC <69664712+JamesDPC@users.noreply.github.com>
2025-09-18 01:50:21 +0000

    [php-cs-fixer] Automated updates generated by php-cs-fixer configuration

--- src/Exceptions/InvalidSourceUrlException.php
+++ src/Exceptions/InvalidSourceUrlException.php
@@ -6,7 +6,6 @@ namespace NSWDPC\Embed\Exceptions;
  * This exception is thrown when the embed source url is invalid or
  * missing required parts
  */
-class InvalidSourceUrlException extends \Exception {
-
-
+class InvalidSourceUrlException extends \Exception
+{
 }
